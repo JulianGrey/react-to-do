1e8d920961ed15b2d872a78e7f7fb932464884dd
Julian Grey <julianrgrey@gmail.com>
2025-05-19 16:08:33 +0100

    Reorganise styling of ToDo
    Restructuring to prepare for a better method of displaying the description section.

--- src/components/ToDo.jsx
+++ src/components/ToDo.jsx
@@ -1,12 +1,23 @@
+import { useState } from 'react';
+
 import './ToDo.scss';
 
 export default function ToDo({toDo, onRemoveToDo}) {
   const { title, description } = toDo;
 
+  const [isOpen, setIsOpen] = useState(false);
+
+  function handleIsOpen() {
+    setIsOpen(() => !isOpen);
+  }
+
   return (
     <li className='to-do-entry'>
-      {description ? `${title} - ${description}` : `${title}`}
-      <button onClick={() => onRemoveToDo(toDo)}>Remove</button>
+      <div onClick={handleIsOpen} className={`entry-title${isOpen ? ' active' : ''}`}>
+        {title}
+        <button onClick={() => onRemoveToDo(toDo)}>Remove</button>
+      </div>
+      {description && <span className='entry-description'>{description}</span>}
     </li>
   );
 }
