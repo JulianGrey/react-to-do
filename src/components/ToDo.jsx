a8da1f314a285ce7c0d9dedfa6ca7c0119ed5726
Julian Grey <julianrgrey@gmail.com>
2025-05-19 13:14:54 +0100

    Improve styling

--- src/components/ToDo.jsx
+++ src/components/ToDo.jsx
@@ -1,8 +1,10 @@
+import './ToDo.scss';
+
 export default function ToDo({toDo, onRemoveToDo}) {
   const { title, description } = toDo;
 
   return (
-    <li>
+    <li className='to-do-entry'>
       {description ? `${title} - ${description}` : `${title}`}
       <button onClick={() => onRemoveToDo(toDo)}>Remove</button>
     </li>
